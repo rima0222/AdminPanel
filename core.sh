27a0cb751621fb76de16c580a13e9bc1d77cc6d7
rima0222 <amirwork150@gmail.com>
2026-02-03 20:25:51 +0330

    Update core.sh

--- core.sh
+++ core.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-DB_FILE="/root/users.db"
+# Smart Guardian - rima0222
+
 case "$1" in
     monitor)
         while true; do
-            sqlite3 "$DB_FILE" "SELECT username, limit_login FROM users WHERE status='active';" | while read -r row; do
-                u=$(echo $row | cut -d'|' -f1); l=$(echo $row | cut -d'|' -f2)
-                while [ $(ps -u "$u" | grep sshd | wc -l) -gt "$l" ]; do pkill -u "$u" -o; done
-            done
+            # در اینجا کد Anti-Multi Login قرار می‌گیرد
             sleep 20
         done
         ;;
     backup)
-        tar -czf /root/smart_backup.tar.gz "$DB_FILE" /etc/passwd /etc/shadow /etc/ssmtp/ssmtp.conf
-        admin_mail=$(grep "AuthUser" /etc/ssmtp/ssmtp.conf | cut -d'=' -f2)
-        echo "فایل بک‌آپ ۶ ساعته سیستم هوشمند پیوست شد." | mail -s "Smart System Backup" -A /root/smart_backup.tar.gz "$admin_mail"
+        # ارسال بک‌آپ به ایمیلی که در پنل ست کردید
+        EMAIL=$(grep "root=" /etc/ssmtp/ssmtp.conf | cut -d= -f2)
+        if [ ! -z "$EMAIL" ]; then
+            echo "Backup of SSH Users Database" | mail -s "SSH Backup $(date)" -A /root/users.db $EMAIL
+        fi
         ;;
 esac
