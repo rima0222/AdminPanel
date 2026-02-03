4168b5acf36caf8b1ee67595995333aa8417d796
rima0222 <amirwork150@gmail.com>
2026-02-03 19:52:56 +0330

    Update panel.py

--- panel.py
+++ panel.py
@@ -25,4 +25,4 @@ def index():
 
 if __name__ == '__main__':
     os.system("screen -dmS smart_monitor bash /root/core.sh monitor")
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=5000)
