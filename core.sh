c561906fbe0f2a92d91aa4059480305237285710
rima0222 <amirwork150@gmail.com>
2026-02-06 08:21:30 +0330

    Update core.sh

--- core.sh
+++ core.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-# Smart Guardian - Anti Multi-Login
-
 while true; do
     sqlite3 /root/users.db "SELECT username, limit_login FROM users;" | while read -r row; do
-        u=$(echo "$row" | cut -d'|' -f1)
-        lim=$(echo "$row" | cut -d'|' -f2)
+        u=$(echo "$row" | cut -d'|' -f1); lim=$(echo "$row" | cut -d'|' -f2)
         [ -z "$lim" ] && lim=1
-        
         count=$(ps -u "$u" | grep sshd | wc -l)
         if [ "$count" -gt "$lim" ]; then
             pkill -u "$u" -old
